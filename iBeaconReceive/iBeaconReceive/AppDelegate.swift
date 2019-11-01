2d1d4c4151dece112d3dc1cae95dbc9e38489214
Shun Kumagai <b1017090@kumagaishun-no-MacBook-Pro.local>
2019-11-01 19:13:15 +0900

    iBeaconReceive

---
+++ iBeaconReceive/iBeaconReceive/AppDelegate.swift
@@ -0,0 +1,50 @@
+//
+//  AppDelegate.swift
+//  iBeaconReceive
+//
+//  Created by 熊谷峻 on 2019/10/18.
+//  Copyright © 2019 Shun Kuma. All rights reserved.
+//
+
+import UIKit
+
+
+@available(iOS 13.0, *)
+@available(iOS 13.0, *)
+@available(iOS 13.0, *)
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+  // MARK: UISceneSession Lifecycle
+
+  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    // Called when a new scene session is being created.
+    // Use this method to select a configuration to create the new scene with.
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
+
+
+  func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    // Called when the user discards a scene session.
+    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+  }
+/*
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+         // Override point for customization after application launch.
+  
+         //ローカル通知（iOS8から必要となる。今回はバナーのみ。）
+         application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: UIUserNotificationType.Alert , categories: nil))
+  
+         return true
+     }
+*/
+}
+
