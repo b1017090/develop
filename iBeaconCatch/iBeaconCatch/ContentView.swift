c37bda393cacf0bbaa3db188c2f5f92b52eb1ec4
Shun Kumagai <b1017090@kumagaishun-no-MacBook-Pro.local>
2019-11-01 19:18:01 +0900

    iBeaconCatch

---
+++ iBeaconCatch/iBeaconCatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iBeaconCatch
+//
+//  Created by 熊谷峻 on 2019/10/18.
+//  Copyright © 2019 Shun Kuma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
