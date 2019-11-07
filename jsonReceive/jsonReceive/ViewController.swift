bae2761f2b35e2fe8cb24576968547b235f3b908
Shun Kumagai <b1017090@kumagaishun-no-MacBook-Pro.local>
2019-11-07 14:00:00 +0900

    jsonReceive

--- jsonReceive/jsonReceive/ViewController.swift
+++ jsonReceive/jsonReceive/ViewController.swift
@@ -10,32 +10,41 @@ import UIKit
 import Foundation
 
 class ViewController: UIViewController {
+  
+       //json構造体のグローバル変数
+       struct JsonSample : Codable{
+         let uuid : String?
+         let key : String?
+         let name : String?
+         let profile : String?
+  }
 
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-      struct JsonSample : Codable{
-          let uuid : String
-          let key : String
-      }
+      JsonGet()//Jsonデータを取ってくる関数
 
-      let listUrl = "http://35.184.233.225:8080/uuidtest"
-      guard let url = URL(string: listUrl) else { return }
-
-      URLSession.shared.dataTask(with: url) { (data, response, error) in
+  }
+     
+        func JsonGet(){
+        let listUrl = "http://35.184.233.225:8080/sendtest"//jsonを取得するwebページのURL
+        guard let url = URL(string: listUrl) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+        //errorが起きたとき
           if error != nil {
-              print(error!.localizedDescription)
-          
-          }else {
-            let sample = String(data: data!, encoding: .utf8)!
-              
-              let json = try? JSONDecoder().decode(JsonSample.self, from: sample.data(using: .utf8)!)
-            print(json?.uuid as Any)
-            
-        }
+                print(error!.localizedDescription)
+            }
+            guard let data = data else { return }
+
+          let json = try? JSONDecoder().decode([JsonSample].self, from: data)//jsonのデコード
+          let encoder = JSONEncoder()
+          encoder.outputFormatting = .prettyPrinted//ここでOptional()を外す
+          let encoded = try! encoder.encode(json)
+          print(String(data: encoded, encoding: .utf8)!)
+        }.resume()
+  }
 }
-}
-}
-          
+
 
 
