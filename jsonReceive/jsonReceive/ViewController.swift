0fcaad439db95bd31498c65fee7c0df16fde905a
Shun Kumagai <b1017090@kumagaishun-no-MacBook-Pro.local>
2019-11-07 17:48:09 +0900

    jsonReceive

--- jsonReceive/jsonReceive/ViewController.swift
+++ jsonReceive/jsonReceive/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Foundation
 
 class ViewController: UIViewController {
-  
+       var UserNumber = 1
        //json構造体のグローバル変数
        struct JsonSample : Codable{
          let uuid : String?
@@ -18,15 +18,15 @@ class ViewController: UIViewController {
          let name : String?
          let profile : String?
   }
-
+// var Jsondata = JsonSample(uuid: "a", key: "a", name: "a", profile: "a")
   
     override func viewDidLoad() {
         super.viewDidLoad()
       JsonGet()//Jsonデータを取ってくる関数
-
+      print();
   }
      
-        func JsonGet(){
+ func JsonGet(){
         let listUrl = "http://35.184.233.225:8080/sendtest"//jsonを取得するwebページのURL
         guard let url = URL(string: listUrl) else { return }
         
@@ -37,11 +37,13 @@ class ViewController: UIViewController {
             }
             guard let data = data else { return }
 
-          let json = try? JSONDecoder().decode([JsonSample].self, from: data)//jsonのデコード
+          let json = try? JSONDecoder().decode([JsonSample].self, from:data)//jsonのデコード
           let encoder = JSONEncoder()
-          encoder.outputFormatting = .prettyPrinted//ここでOptional()を外す
-          let encoded = try! encoder.encode(json)
-          print(String(data: encoded, encoding: .utf8)!)
+          encoder.outputFormatting = .prettyPrinted//ここでprint出力を整形する
+        //  let encoded = try! encoder.encode(json)
+          print(json![self.UserNumber].uuid as Any,json![self.UserNumber].key as Any,json![self.UserNumber].name as Any,json![self.UserNumber].profile as Any)
+
+          //print(String(data: encoded, encoding: .utf8)!)
         }.resume()
   }
 }
